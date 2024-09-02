9a9a9c70253312f7527af8fe8833613cca695b2a
syncsoftco <syncboard@googlegroups.com>
2024-09-02 14:11:20 -0400

    Update start-tailscale-and-caddy.sh

--- start-tailscale-and-caddy.sh
+++ start-tailscale-and-caddy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# Start the Tailscale daemon
+# start-tailscale-and-caddy.sh
+
+# Start the Tailscale daemon in the background
 /usr/local/bin/tailscaled &
 
 # Wait for the Tailscale daemon to start
 sleep 5
 
-# Bring the Tailscale interface up
+# Bring the Tailscale interface up using the provided auth key
 /usr/local/bin/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=tailscale-gateway
 
-# Dynamically generate the Caddyfile
+# Use the PORT environment variable provided by Cloud Run, default to 8080 if not set
+PORT=${PORT:-8080}
+
+# Dynamically generate the Caddyfile based on the environment variables
 cat <<EOF > /etc/caddy/Caddyfile
-:8080 {
+:${PORT} {
     reverse_proxy ${TARGET_SERVICE}:${TARGET_PORT}
 }
 EOF
 
-# Start the Caddy server
+# Start the Caddy web server, which listens on the port expected by Cloud Run
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
